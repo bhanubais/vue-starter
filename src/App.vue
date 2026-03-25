94bf0644acff0cb0c01513c4db929ed0b48b06bb
bhanubais <bhanubais@gmail.com>
2026-03-25 06:49:14 +0530

    Refactor App.vue to use increment function for button click and add main.js for app initialization

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <script setup>
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 
+// reactive state
 const count = ref(0);
+
+function increment() {
+	count.value++;
+}
+
+onMounted(() => {
+	console.log(`The initial count is ${count.value}.`);
+});
 </script>
 
 <template>
-<button @click="count++" class="btn btn-outline-primary">Count: {{ count }}</button>
+<button @click="increment" class="btn btn-outline-primary">Count: {{ count }}</button>
 </template>
 
 <style scoped>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import "@/scss/styles.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+
+const app = createApp(App);
+app.mount("#app");
