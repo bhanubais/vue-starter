930a70c05afa13aa160aefa76e97da850acca1f5
bhanubais <bhanubais@gmail.com>
2026-03-29 00:25:28 +0530

    Introduced vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,6 @@
 <script setup>
-import { onMounted, ref } from "vue";
-
-const loading = ref(true);
-const err = ref(null);
-const data = ref(null);
-const access_token = ref(
-	localStorage.access_token ??
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIzNCIsInJvbGUiOiJTVFVERU5UIiwidHlwZSI6ImFjY2VzcyIsImlhdCI6MTc3NDY5NjExMSwiZXhwIjoxNzc0Njk5NzExLCJ2ZXIiOjF9.rjffFof8OYDcfOxQCDU_LlmhSFjDyMVNo-xNy5Gf_iQ",
-);
-
-const URL = "/api/v1/student/drives?sort=deadline_asc&page=1&per_page=20";
-
-const payload = {
-	method: "GET",
-	headers: {
-		accept: "application/json",
-		"Content-Type": "application/json",
-		Authorization: `Bearer ${access_token.value}`,
-	},
-};
-
-const fetchData = async () => {
-	loading.value = true;
-	try {
-		const response = await fetch(URL, payload);
-		data.value = await response.json();
-		access_token.value = data.value.access_token;
-	} catch (error) {
-		err.value = error;
-	} finally {
-		loading.value = false;
-	}
-};
-
-onMounted(async () => {
-	await fetchData();
-	localStorage.setItem("access_token", access_token.value);
-});
 </script>
 <template>
-<div v-if="loading" class="spinner-border" role="status">
-  <span class="visually-hidden">Loading...</span>
-</div>
-<div v-else-if="err" class="alert alert-danger">
-	Data cound't fetched!
-	<div>{{ err }}</div>
-</div>
-<pre v-else>
-	{{ data }}
-</pre>
+<p>Welcome to the Application</p>
+<router-view></router-view>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import "@/scss/styles.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { createApp } from "vue";
-import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "@/App.vue";
+import About from "@/view/About.vue";
+import Home from "@/view/Home.vue";
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{ path: "/", name: "Home", component: Home },
+		{ path: "/about", name: "About", component: About },
+	],
+});
 
 const app = createApp(App);
+app.use(router);
 app.mount("#app");
